cb231a57d9d91f341c70dc81a5764f80775066ad
egoulan <eddygoulan@gmail.com>
2025-08-09 17:54:36 +0200

    update package

--- src/services/jobs/update-sports.service.ts
+++ src/services/jobs/update-sports.service.ts
@@ -1,12 +1,19 @@
-import { supabase } from "../../config/supabase";
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 import { SportUtils } from "../../utils/sport.utils";
 import { OddsApiService } from "../odds-api.service";
+
+dotenv.config();
 
 export class UpdateSportsService {
   static async execute() {
     try {
       const sportsResponse = await OddsApiService.getSports();
       const sports = SportUtils.filterValidSports(sportsResponse);
+      const supabase = createClient(
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_ANON_KEY!
+      );
       await supabase
         .from("sports")
         .upsert(sports, { onConflict: "key", ignoreDuplicates: true });
