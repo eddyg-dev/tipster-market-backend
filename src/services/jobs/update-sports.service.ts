bd16cd902e72e8b7f231e8960f5b02709fbbc8f8
egoulan <eddygoulan@gmail.com>
2025-08-09 18:05:27 +0200

    update script

--- src/services/jobs/update-sports.service.ts
+++ src/services/jobs/update-sports.service.ts
@@ -1,5 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
+import { supabase } from "../../config/supabase";
 import { SportUtils } from "../../utils/sport.utils";
 import { OddsApiService } from "../odds-api.service";
 
@@ -10,14 +10,20 @@ export class UpdateSportsService {
     try {
       const sportsResponse = await OddsApiService.getSports();
       const sports = SportUtils.filterValidSports(sportsResponse);
-      const supabase = createClient(
-        process.env.SUPABASE_URL!,
-        process.env.SUPABASE_ANON_KEY!
-      );
-      await supabase
-        .from("sports")
-        .upsert(sports, { onConflict: "key", ignoreDuplicates: true });
-      return { success: true, message: `${sports.length} sports mis à jour` };
+
+      const { data, error } = await supabase.from("sports").upsert(sports, {
+        onConflict: "key",
+      });
+
+      if (error) {
+        console.error("Erreur insertion sports:", error);
+      } else {
+        console.log(`${sports.length} sports traités avec succès`);
+      }
+      return {
+        success: true,
+        message: `${sports.length} sports mis à jour`,
+      };
     } catch (error) {
       console.error("Erreur générale:", error);
       return {
